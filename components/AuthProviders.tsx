4d5ed98acc99cf32047609defde01b01b51d85a2
ranked78 <click2edit@gmail.com>
2023-08-11 21:43:29 +0800

    49:37

--- components/AuthProviders.tsx
+++ components/AuthProviders.tsx
@@ -1,7 +1,33 @@
-import React from "react";
+"use client";
+
+import { getProviders, signIn } from "next-auth/react";
+import { useState, useEffect } from "react";
+
+type Provider = {
+  id: string;
+  name: string;
+  type: string;
+  signinUrl: string;
+  callbackUrl: string;
+  signinUrlParams?: Record<string, string> | null;
+};
+
+type Providers = Record<string, Provider>;
 
 const AuthProviders = () => {
-  return <div>AuthProviders</div>;
+  const [providers, setProviders] = useState<Providers | null>(null);
+
+  if (providers) {
+    return (
+      <div>
+        {Object.values(providers).map((provider: Provider, i: number) => (
+          <button key={i}>{provider.id}</button>
+        ))}
+      </div>
+    );
+  }
+
+  return null; // Return null if providers is null
 };
 
 export default AuthProviders;

976f021d072b92e5acfd4f00f759a58d062fa3e7
ranked78 <click2edit@gmail.com>
2023-08-13 13:40:19 +0800

    sessions updated

--- lib/session.ts
+++ lib/session.ts
@@ -1,3 +1,36 @@
 import {  getServerSession  } from 'next-auth/next';
 import {NextAuthOptions,User} from 'next-auth';
-import {AdapterUser} from 'next-auth/adapters';
+import { AdapterUser } from "../node_modules/next-auth/src/adapters";
+import GoogleProvider from 'next-auth/providers/google';
+import jsonwebtoken from 'jsonwebtoken';
+import {JWT} from 'next-auth/jwt';
+
+
+export const authOptions: NextAuthOptions = {
+    providers: [
+        GoogleProvider({
+          clientId: process.env.GOOGLE_CLIENT_ID!,
+          clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
+        }),
+      ],
+      jwt:{
+        encode: ({secret, token})=>{
+
+        },
+        decode: async ()=>{
+            
+        }
+      },
+      theme: {
+        colorScheme: 'light',
+        logo: '/logo.png'
+      },
+      callbacks:{
+        async session({session}){
+
+        },
+        async signIn({user}){
+            
+        }
+      }
+}

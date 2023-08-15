43411cc9168c1057b57a31f3b0160f57781c9e83
ranked78 <click2edit@gmail.com>
2023-08-15 18:50:15 +0800

    module Resolution

--- lib/session.ts
+++ lib/session.ts
@@ -1,6 +1,6 @@
 import {  getServerSession  } from 'next-auth/next';
 import {NextAuthOptions,User} from 'next-auth';
-import { AdapterUser } from "../node_modules/next-auth/src/adapters";
+import {AdapterUser} from 'next-auth/adapters';
 import GoogleProvider from 'next-auth/providers/google';
 import jsonwebtoken from 'jsonwebtoken';
 import {JWT} from 'next-auth/jwt';
